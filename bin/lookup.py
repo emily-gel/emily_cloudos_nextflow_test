6a06b9cf109c8b401174bbd3d4d898f627d4d56b
Emily Perry <95911323+emily-gel@users.noreply.github.com>
2025-05-27 13:57:01 +0100

    Update lookup.py

--- bin/lookup.py
+++ bin/lookup.py
@@ -11,20 +11,20 @@ import numpy as np
     required=True,
 )
 
-def query_to_df(sql_query, database):
-    connection = psycopg2.connect(
-        dbname = "gel_clinical_cb_sql_pro",
-        options = f"-c search_path=source_data_100kv16_covidv4",
-        host = "clinical-cb-sql-pro.cfe5cdx3wlef.eu-west-2.rds.amazonaws.com",
-        port = 5432,
-        password = 'anXReTz36Q5r',
-        user = 'jupyter_notebook'
-    )
-    return(pd.read_sql_query(sql_query, connection))
-
 def query(participant_id: int):
 
     version = "source_data_100kv16_covidv4"
+    
+    def query_to_df(sql_query, database):
+        connection = psycopg2.connect(
+            dbname = "gel_clinical_cb_sql_pro",
+            options = f"-c search_path=source_data_100kv16_covidv4",
+            host = "clinical-cb-sql-pro.cfe5cdx3wlef.eu-west-2.rds.amazonaws.com",
+            port = 5432,
+            password = 'anXReTz36Q5r',
+            user = 'jupyter_notebook'
+        )
+        return(pd.read_sql_query(sql_query, connection))
     
     hes_sql = (f'''
         SELECT participant_id, arrivaldate, diag_all
@@ -33,7 +33,6 @@ def query(participant_id: int):
         ''')
 
     hes_query = query_to_df(hes_sql, version)
-    hes_query
 
     out = open("output.txt", "w") 
     out.write(hes_query) 
