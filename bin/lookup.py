9598e08201bc227db354fe5fbd81ca1fb9196b01
Emily Perry <95911323+emily-gel@users.noreply.github.com>
2025-06-13 14:56:20 +0100

    Update lookup.py

--- bin/lookup.py
+++ bin/lookup.py
@@ -3,7 +3,7 @@ import click
 from sqlalchemy import create_engine, event, text
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 @click.command()
 @click.option( "--participant_id", type=int, required=True)
@@ -513,21 +513,21 @@ def query(participant_id, ae_ana, ae_con, ae_inv, ae_side, ae_tre, icd10, opcs, 
         table = table.sort_values(by=['date'])
         return(table)
 
-    def clinical_graph(participant_id):
-        input = all_clinical_table(participant_id).dropna()
-        height = len(pd.unique(input['event']))/5
-        input['event_trunc'] = input['event'].apply(lambda x: x[:20])
+    # def clinical_graph(participant_id):
+    #     input = all_clinical_table(participant_id).dropna()
+    #     height = len(pd.unique(input['event']))/5
+    #     input['event_trunc'] = input['event'].apply(lambda x: x[:20])
         
-        plt.plot_date(input['date'], input['event_trunc'], xdate=True, ydate=False)
-        plt.xticks(rotation=70)
-        plt.figure(figsize=(10,height))
-        groups = input.groupby('source')
-        for name, group in groups:
-            plt.plot(group.date, group.event_trunc, marker='o', linestyle='', markersize=3, label=name)
-        plt.legend()
-        plt.xlabel('Date of event')
-        plt.ylabel('Event')
-        plt.savefig(f'{participant_id}_graph.png', bbox_inches='tight')
+    #     plt.plot_date(input['date'], input['event_trunc'], xdate=True, ydate=False)
+    #     plt.xticks(rotation=70)
+    #     plt.figure(figsize=(10,height))
+    #     groups = input.groupby('source')
+    #     for name, group in groups:
+    #         plt.plot(group.date, group.event_trunc, marker='o', linestyle='', markersize=3, label=name)
+    #     plt.legend()
+    #     plt.xlabel('Date of event')
+    #     plt.ylabel('Event')
+    #     plt.savefig(f'{participant_id}_graph.png', bbox_inches='tight')
 
     def html (participant_id):
         out = open(f"{participant_id}_output.html", "x")
@@ -564,8 +564,8 @@ def query(participant_id, ae_ana, ae_con, ae_inv, ae_side, ae_tre, icd10, opcs, 
         out.write(genomic(participant_id))    
         out.write("<h1>Clinical data</h1>")
         out.write(all_clinical_table(participant_id).to_html()) 
-        clinical_graph(participant_id)
-        out.write(f"<img src={participant_id}_graph.png>")
+        # clinical_graph(participant_id)
+        # out.write(f"<img src={participant_id}_graph.png>")
         out.write("</body>")
         out.close()
     html(participant_id)
